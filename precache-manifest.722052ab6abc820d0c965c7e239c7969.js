d940aba646a76df1b857108a7b0a9dd7a3a942e1
6y6a <gubinalexeymain@gmail.com>
2020-01-02 16:50:22 +0300

    Updates

---
+++ precache-manifest.722052ab6abc820d0c965c7e239c7969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca95b7fb8472cfa4bad063538669a5ec",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "2b5aa68e44aaaec92189",
+    "url": "/monsters/static/css/main.6a55ecfc.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/monsters/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b5aa68e44aaaec92189",
+    "url": "/monsters/static/js/main.ffe9a0fe.chunk.js"
+  },
+  {
+    "revision": "f3f0459f8a3156a81a29",
+    "url": "/monsters/static/js/runtime-main.0e8a63f8.js"
+  }
+]);
